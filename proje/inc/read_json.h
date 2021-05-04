5785b8694e661aa0f93978704dd8593481d6d7eb
Ayberk KÖSE <51715983+MrAlbino@users.noreply.github.com>
2021-05-04 11:20:09 +0300

    error-handling
    i added error handling about the bad json format.

--- proje/inc/read_json.h
+++ proje/inc/read_json.h
@@ -1,10 +1,10 @@
 /**
-Sistem Programlama Proje Ödevi
+Sistem Programlama Proje Odevi
 * @file : read_json.h
 * @description : reading .kilit file and extracting requested values.
 * @assignment : System Programming Project
 * @startdate : 01.05.2021
-* @author : Ayberk KÖSE
+* @author : Ayberk KOSE
 */
 #ifndef READ_JSON
 #define READ_JSON
@@ -17,12 +17,8 @@ Sistem Programlama Proje Ödevi
 
 JRB create_tree_for_decode();
 JRB create_tree_for_encode();
-
 void remove_spaces(char *str);
 char *extract_quotes(char *s);
 int parse_then_add(char *data, JRB tree, char mode);
 int file_exists(IS is);
 #endif
-
-
-
